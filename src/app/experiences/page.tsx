56189bdc13ab6234c02d033bc623c763d2b98158
Anadudev <anadudev@gmail.com>
2025-01-21 14:10:49 +0100

    Update: add new ProjectPage component with placeholder content

---
+++ src/app/experiences/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectPage = () => {
+  return (
+    <div className="p-4 pt-36 text-black dark:text-slate-300">
+      <div className="border"></div>
+      Coming soon...!
+    </div>
+  );
+};
+
+export default ProjectPage;
