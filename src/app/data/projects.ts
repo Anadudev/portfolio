6d67e913c67816ffa33dd6db89532fe1b43e59a6
Anadudev <anadudev@gmail.com>
2025-01-21 15:50:10 +0100

    Update: rename and update project details for MiraTends and Porthree for consistency and clarity

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,12 +1,12 @@
 export const works1 = [
   {
-    name: "Anadu Portfolio",
+    name: "MiraTends",
     description:
-      "A personal portfolio website built with Next.js and Tailwind CSS.",
-    link: "/",
-    github: "https://github.com/Anadudev/portfolio",
-    thumbnail: "/projects/work1.png",
-    tags: [1, 2, 5, 11, 7, 9, 16],
+      "A comprehensive online market place where you can buy, sell and manage products.",
+    link: "https://mira-trends.vercel.app/",
+    // github: "#",
+    thumbnail: "/projects/work3.png",
+    tags: [1, 8, 12, 2, 4, 5, 7, 9, 14, 16, 3, 17],
   },
   {
     name: "",
@@ -35,23 +35,7 @@ export const works1 = [
 ];
 
 export const works2 = [
-  {
-    name: "MiraTends",
-    description:
-      "A comprehensive online market place where you can buy, sell and manage products.",
-    link: "https://mira-trends.vercel.app/",
-    // github: "#",
-    thumbnail: "/projects/work3.png",
-    tags: [1, 8, 12, 2, 4, 5, 7, 9, 14, 16, 3, 17],
-  },
-  {
-    name: "",
-    description: "",
-    link: "",
-    // github: "",
-    thumbnail: "",
-    tags: [],
-  },
+
   {
     name: "Porthree",
     description:
@@ -60,8 +44,25 @@ export const works2 = [
     github: "https://github.com/Anadudev/porthree",
     thumbnail: "/projects/work5.png",
     tags: [1, 2, 3, 5, 12, 6, 16, 13, 10, 7],
+  }, {
+    name: "",
+    description: "",
+    link: "",
+    // github: "",
+    thumbnail: "",
+    tags: [],
   },
-// props.github
+  {
+    name: "Anadu Portfolio",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    link: "/",
+    github: "https://github.com/Anadudev/portfolio",
+    thumbnail: "/projects/work1.png",
+    tags: [1, 2, 5, 11, 7, 9, 16],
+  },
+
+  // props.github
   {
     name: "",
     description: "",
