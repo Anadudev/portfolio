b20d5563d1b68652d2d9d6fd3809f0c2172a790c
Anadudev <anadudev@gmail.com>
2025-02-12 15:07:45 +0100

    Update: enhance infinite scroll component with cursor pointer and title attributes; adjust animation duration

--- src/app/components/tools-infinite-scroll.tsx
+++ src/app/components/tools-infinite-scroll.tsx
@@ -2,14 +2,14 @@ import { ToolsInfiniteScrollProps, techStack } from '@/app/types/types'
 
 const ToolsInfiniteScroll = (props: ToolsInfiniteScrollProps) => (
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-        <ul className={`flex items-center justify-center md:justify-start ${props.reverse ? 'animate-infinite-scroll-right' : 'animate-infinite-scroll-left'}`}>
+        <ul className={`flex items-center justify-center md:justify-start  ${props.reverse ? 'animate-infinite-scroll-right' : 'animate-infinite-scroll-left'}`}>
             {props.item.map((stack: techStack, index: number) => (
-                <li className={`mx-3`} key={index}>{<stack.icon className='text-2xl' />}</li>
+                <li className={`mx-3 cursor-pointer`} title={stack.text} key={index}>{<stack.icon className='text-2xl' />}</li>
             ))}
         </ul>
         <ul className={`flex items-center justify-center md:justify-start ${props.reverse ? 'animate-infinite-scroll-right' : 'animate-infinite-scroll-left'}`}>
             {props.item.map((stack: techStack, index: number) => (
-                <li className={`mx-3`} key={index}>{<stack.icon className='text-2xl' />}</li>
+                <li className={`mx-3 cursor-pointer`} title={stack.text} key={index}>{<stack.icon className='text-2xl' />}</li>
             ))}
         </ul>
     </div>
