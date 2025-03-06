e10a6f81fb1ba583876f5c9ca891a2588c997c6b
Anadudev <anadudev@gmail.com>
2025-03-06 08:13:11 +0100

    Update: refactor technology data structure for consistency in icon representation

--- src/app/data/technologies.tsx
+++ src/app/data/technologies.tsx
@@ -1,28 +1,35 @@
+// import { TiHtml5 } from "react-icons/ti";
+// import { FaCss3Alt } from "react-icons/fa6";
+// import { SiJavascript } from "react-icons/si";
+// import { BiLogoTypescript } from "react-icons/bi";
+// import { FaReact } from "react-icons/fa";
+
+
 export const frontendTechnologies = [
-	{text: "HTML5", icon: TiHtml5},
-	{text: "CSS3", icon: IoLogoCss3},
-	{text: "JavaScript (ES6+)", icon: SiJavascript},
-	{text: "TypeScript", icon: SiTypescript},
-	{text: "React", icon: FaReact},
-	{text: "Next", icon: TbBrandNextjs},
-	{text: "Tailwind", icon: SiTailwindcss},
-	{text: "Socket.io-Client", icon: FaGitAlt},
-]
+	{ text: "HTML5", icon: `TiHtml5` },
+	{ text: "CSS3", icon: `IoLogoCss3` },
+	{ text: "JavaScript (ES6+)", icon: `SiJavascript` },
+	{ text: "TypeScript", icon: `SiTypescript` },
+	{ text: "React", icon: `FaReact` },
+	{ text: "Next", icon: `TbBrandNextjs` },
+	{ text: "Tailwind", icon: `SiTailwindcss` },
+	{ text: "Socket.io-Client", icon: `FaGitAlt` },
+];
 
 export const backendTechnologies = [
-	{text: "Node.js", icon: RiNodejsLine},
-	{text: "Express.js", icon: FaGitAlt},
-	{text: "Socket.io", icon: FaGitAlt},
-	{text: "MongoDB", icon: FaGitAlt},
-	{text: "PostgreSQL", icon: FaGitAlt},
-	{text: "Prisma", icon: FaGitAlt},
-	{text: "TypeScript", icon: FaGitAlt},
-	{text: "JWT (for authentication)", icon: FaGitAlt},
-]
+	{ text: "Node.js", icon: `RiNodejsLine` },
+	{ text: "Express.js", icon: `FaGitAlt` },
+	{ text: "Socket.io", icon: `FaGitAlt` },
+	{ text: "MongoDB", icon: `FaGitAlt` },
+	{ text: "PostgreSQL", icon: `FaGitAlt` },
+	{ text: "Prisma", icon: `FaGitAlt` },
+	{ text: "TypeScript", icon: `FaGitAlt` },
+	{ text: "JWT (for authentication)", icon: `FaGitAlt` },
+];
 
 export const deploymentTechnologies = [
-	{text: "Heroku", icon: FaGitAlt},
-	{text: "Netlify", icon: FaGitAlt},
-	{text: "Vercel", icon: FaGitAlt},
-	{text: "Render", icon: FaGitAlt},
-]
+	{ text: "Heroku", icon: `FaGitAlt` },
+	{ text: "Netlify", icon: `FaGitAlt` },
+	{ text: "Vercel", icon: `FaGitAlt` },
+	{ text: "Render", icon: `FaGitAlt` },
+];
